e9aa4b5b7f2b46c9b23ca88e448bfbde0de2f389
lixiuhu <lixiuhu_lee@163.com>
2018-10-27 02:51:47 +0800

    update docker-compose.yml and start.sh script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir jenkins-data
+# The user in container is jenkins(1000:1000). The host should create a user uid:gid=1000:1000
+chown -R 1000:1000 jenkins-data
+docker-compose up -d
+while true
+do
+    if [ -f "./jenkins-data/secrets/initialAdminPassword" ];then
+        echo `cat jenkins-data/secrets/initialAdminPassword`
+        break
+    else
+        sleep 5
+    fi
+done
